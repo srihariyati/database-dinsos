ca66a549b561060425f133d3c08f03a819a161ca
srihariyati(●'◡'●) <sri.hr@mhs.unsyiah.ac.id>
2022-08-23 09:51:50 +0700

    Installing Bootstrap 5

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+   
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <h1>This is example from ItSolutionStuff.com</h1>
+</body>
+</html>
