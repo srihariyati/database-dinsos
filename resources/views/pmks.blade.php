8c33c985a4d4059e574a063a9f6378b74ea3c96d
choirunisa01 <choirunisaromadoni01@gmail.com>
2022-08-30 09:24:45 +0700

    Create pmks.blade.php

---
+++ resources/views/pmks.blade.php
@@ -0,0 +1,17 @@
+@extends('template')
+@section('title','PMKS')
+
+@section('content')
+    <div id="card-satu">
+        <div class="row">
+            <div class="col-md-4  ">
+                <h4 class=" fw-bold" >Data PMKS</h4>
+            </div>
+                    
+            <div class="col-md-6">
+                <button type="submit" name="login" class="tombol btn-group text-white fw-bold form-control btn-lg mt-4" style="background-color: #4B7BE5">Login</button>
+            </div>
+        </div>
+    </div>
+
+@endsection
