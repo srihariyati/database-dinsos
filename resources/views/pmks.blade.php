d7770cb28c8832b56555ecb8fa2f2a59d9564600
Fiya Yufita <64641420+fiyayufita@users.noreply.github.com>
2022-08-30 12:03:14 +0700

    update pmks

--- resources/views/pmks.blade.php
+++ resources/views/pmks.blade.php
@@ -3,14 +3,35 @@
 
 @section('content')
     <div id="card-satu">
-        <div class="row">
+        <div id="data" class="row">
             <div class="col-md-4  ">
                 <h4 class=" fw-bold" >Data PMKS</h4>
             </div>
                     
-            <div class="col-md-6">
-                <button type="submit" name="login" class="tombol btn-group text-white fw-bold form-control btn-lg mt-4" style="background-color: #4B7BE5">Login</button>
+            <div id="button"class="col-md-4">
+                <button type="submit" name="login" class="tombol btn-group text-white fw-bold form-control btn-lg mt-3" style="background-color: #1CCE2E">Tambah</button>
             </div>
+
+            <form>
+                <div class="form-row">
+                    <div class="form-group col-md-4">
+                        <label for="inputState">State</label>
+                        <select id="inputState" class="form-control">
+                            <option selected>Choose...</option>
+                            <option>...</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group col-md-4">
+                        <label for="inputState">State</label>
+                        <select id="inputState" class="form-control">
+                            <option selected>Choose...</option>
+                            <option>...</option>
+                        </select>
+                    </div>
+                </div>
+            </form>
+
         </div>
     </div>
 
