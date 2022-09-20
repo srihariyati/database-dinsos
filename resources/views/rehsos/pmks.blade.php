f81002df4846bdcb423fab3e9c86592ed2ec17a2
srihariyati(●'◡'●) <sri.hr@mhs.unsyiah.ac.id>
2022-09-20 10:09:41 +0700

    Update Controller

--- resources/views/rehsos/pmks.blade.php
+++ resources/views/rehsos/pmks.blade.php
@@ -14,9 +14,7 @@
                             <a class="btn btn-success" id="tambah" href="{{ url('/tambahpmks')}}" role="button">Tambah</a>
                     </div>
                 </div>
-            </div>
-
-
+        </div>
 
             <div class="container">
                 <div class="row" style="margin-top: 1rem; margin-left: -2.4rem">
@@ -83,111 +81,23 @@
                 </div>
             </div>
         <div class="table-responsive" style="height: 250px; font-size:12px;">
-        <table id="tabel-data" class="table table-striped table-bordered" width="100%" cellspacing="0" >
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Kecamatan</th>
-                <th>Desa/Kelurahan</th>
-                <th>Bulan</th>
-                <th>Tahun</th>
-                <th>GLD</th>
-                <th>PENG</th>
-                <th>PUNK</th>
-                <th>ANJAL</th>
-                <th>OT</th>
-                <th>AT</th>
-                <th>PSK</th>
-                <th>WARIA</th>
-                <th>PRIA</th>
-                <th>WANITA</th>
-                <th>TOTAL</th>
-            </tr>
-        </thead>
-
-        <tbody>
-            <tr>
-                <td>Tiger Nixon</td>
-                <td>System Architect</td>
-                <td>Edinburgh</td>
-                <td>61</td>
-                <td>2011/04/25</td>
-                <td>$320,800</td>
-            </tr>
-            <tr>
-                <td>Garrett Winters</td>
-                <td>Accountant</td>
-                <td>Tokyo</td>
-                <td>63</td>
-                <td>2011/07/25</td>
-                <td>$170,750</td>
-            </tr>
-            <tr>
-                <td>Ashton Cox</td>
-                <td>Junior Technical Author</td>
-                <td>San Francisco</td>
-                <td>66</td>
-                <td>2009/01/12</td>
-                <td>$86,000</td>
-            </tr>
-            <tr>
-                <td>Cedric Kelly</td>
-                <td>Senior Javascript Developer</td>
-                <td>Edinburgh</td>
-                <td>22</td>
-                <td>2012/03/29</td>
-                <td>$433,060</td>
-            </tr>
-            <tr>
-                <td>Airi Satou</td>
-                <td>Accountant</td>
-                <td>Tokyo</td>
-                <td>33</td>
-                <td>2008/11/28</td>
-                <td>$162,700</td>
-            </tr>
-            <tr>
-                <td>Brielle Williamson</td>
-                <td>Integration Specialist</td>
-                <td>New York</td>
-                <td>61</td>
-                <td>2012/12/02</td>
-                <td>$372,000</td>
-            </tr>
-            <tr>
-                <td>Herrod Chandler</td>
-                <td>Sales Assistant</td>
-                <td>San Francisco</td>
-                <td>59</td>
-                <td>2012/08/06</td>
-                <td>$137,500</td>
-            </tr>
-            <tr>
-                <td>Rhona Davidson</td>
-                <td>Integration Specialist</td>
-                <td>Tokyo</td>
-                <td>55</td>
-                <td>2010/10/14</td>
-                <td>$327,900</td>
-            </tr>
-            <tr>
-                <td>Colleen Hurst</td>
-                <td>Javascript Developer</td>
-                <td>San Francisco</td>
-                <td>39</td>
-                <td>2009/09/15</td>
-                <td>$205,500</td>
-            </tr>
-            <tr>
-                <td>Sonya Frost</td>
-                <td>Software Engineer</td>
-                <td>Edinburgh</td>
-                <td>23</td>
-                <td>2008/12/13</td>
-                <td>$103,600</td>
-            </tr>
-            
-        </tbody>
+        <table id="tabel-data" class="table table-striped table-bordered" width="100%" cellspacing="0">
+            <thead>
+                <tr>
+                    <th>Column 1</th>
+                    <th>Column 2</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Row 1 Data 1</td>
+                    <td>Row 1 Data 2</td>
+                </tr>
+                <tr>
+                    <td>Row 2 Data 1</td>
+                    <td>Row 2 Data 2</td>
+                </tr>
+            </tbody>
         </table>
         </div>
         </div>
@@ -195,37 +105,44 @@
 
 <script type="text/javascript">
     $(document).ready(function(){
-        $('#tabel-data').DataTable();
-
-        $('#dtHorizontalVerticalExample').DataTable({
-            "scrollX": true,
-            "scrollY": 200,
-        });
-        $('.dataTables_length').addClass('bs-select');
-
         $('#kecamatan').on('change', function(){
-            //ambil value dari id kecamatan
+            //ambil value dari id kecamatan       ]     
             var kecId = this.value;
+            console.log(kecId);
             $('#desa').html('');
+
+            var table =  $('tabel-data').DataTable();
+            table.destroy();
 
             $.ajax({
                 //kirim id ke controller
                 url: '{{ route('getDesa') }}?id_kec='+kecId,
                 type :'get',               
                 success : function(res){
-                    $('#desa').html('<option value="">Pilih Desa</option>');                 
+                    $('#desa').html('<option value="">Pilih Desa</option>'); 
+
+                    console.log(res);               
      
                     $.each(res.desa, function (key, value) {
                         console.log("ini id kec :"+kecId);
                         console.log(value);
                         console.log(res);
                         console.log(value.id_desa, value.nama_desa);
-
+                                               
                         // buat option untuk pilih desa
                         $('#desa').append('<option value="'+ value.id_desa + '">' + value.nama_desa + '</option>');                  
-                        
+                      
                     });
+
+                    console.log(res.kecamatan);
                      // tampilkan dat yang kecamatannya dipilih
+                     table =  $('#tabel-data').DataTable({
+                        "columns": [
+                        { data: "kecId" },
+                        { data:  "kecId"  },
+                        ]
+
+                        });
 
                     $.each(res.datapmks, function(key, value){
                         console.log(value.id_data);
