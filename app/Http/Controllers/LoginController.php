5ef5b4354f36a0a968f9e9a57294a515af3e1a45
choirunisa01 <choirunisaromadoni01@gmail.com>
2022-09-23 09:37:37 +0700

    update

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -47,7 +47,7 @@ class LoginController extends Controller
  
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->intended('/dashboard')->with('success', 'Anda Berhasil Login!');
+            return redirect()->intended('/dashboard');
         }
 
         return back()->with('loginError', 'Login gagal ! Username atau Password Salah');
